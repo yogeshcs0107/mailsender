c5ead1db3d3b1706a1c888618c402ca9f4e69829
yogeshcs0107 <yogeshcs0107@gmail.com>
2019-07-02 02:44:50 +0530

    YS: mail configured

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 "use strict";
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const nodemailer = require("nodemailer");
 const path = require('path');
 
-const port = 4001
+const port = process.env.PORT || 4001
+app.use(bodyParser.json());
 app.use(express.static("src"));
 
 app.get('/', (req, res) => {
@@ -13,32 +15,62 @@ app.get('/', (req, res) => {
 
 
 app.post('/sendmail', (req, res) => {
-    main().then (response => {
+    main(req.body).then (response => {
         res.send(response);
     });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-async function main(){
-const transporter = nodemailer.createTransport({
-    service: 'gmail',//smtp.gmail.com  //in place of service use host...
-    secure: false,//true
-    port: 25,//465
+function main (req) {
+  let data = req || {};
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: false,
+    port: 25,
     auth: {
-      user: 'yogeshcs0107@gmail.com',
-      pass: ''
+      user: '13cabservicesmelbourne@gmail.com',
+      pass: '9876061718'
     }, tls: {
       rejectUnauthorized: false
     }
   });
 
   // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <yogeshcs0107@gmail.com>', // sender address
-    to: "manavmanuprakash@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>" // html body
+  let info = transporter.sendMail({
+    from: '"13 Cab Services Melbourne" <13cabservicesmelbourne@gmail.com>', // sender address
+    to: "13cabservicesmelbourne@gmail.com, yogeshcs0107@gmail.com", // list of receivers
+    subject: "Customer Data", // Subject line
+    html: `
+      <table>
+        <tr>
+          <td>Name</td>:<td>${data.name}</td>
+        </tr>
+        <tr>
+        <td>Phone Number</td>:<td>${data.number}</td>
+        </tr>
+        <tr>
+        <td>Email</td>:<td>${data.email}</td>
+        </tr>
+        <tr>
+        <td>Date of Travel</td>:<td>${data.dateOfTravel}</td>
+        </tr>
+        <tr>
+        <td>Time</td>:<td>${data.time}</td>
+        </tr>
+        <tr>
+        <td>Pickup Location</td>:<td>${data.pickupLoc}</td>
+        </tr>
+        <tr>
+        <td>Destination(To)</td>:<td>${data.destLoc}</td>
+        </tr>
+        <tr>
+        <td>Number of passengers</td>:<td>${data.numberOfPassenger}</td>
+        </tr>
+        <tr>
+        <td>Type of Vehicle</td>:<td>${data.vehicle}</td>
+        </tr>
+      </table>
+    `
   });
   return info;
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,23 @@
-function loadDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-       document.getElementById("demo").innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("POST", "/sendmail", true);
-    xhttp.send();
-  }
+function sendMail() {
+  var data = {
+    name: document.getElementById('name').value,
+    number: document.getElementById('number').value,
+    email: document.getElementById('email').value,
+    dateOfTravel: document.getElementById('dateOfTravel').value,
+    time: document.getElementById('time').value,
+    pickupLoc: document.getElementById('pickupLoc').value,
+    destLoc: document.getElementById('destLoc').value,
+    numberOfPassenger: document.getElementById('numberOfPassenger').value,
+    vehicle: document.getElementById('vehicle').value
+  };
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "/sendmail", true);
+  xhttp.setRequestHeader("Content-Type", "application/json");
+
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.send(JSON.stringify(data));
+}
